572e9d74d7f026b5256bccad3d0b2f3846ea2584
Elton Ho <ho.elton@gmail.com>
2020-09-16 22:39:00 -0700

    fixed problem

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 const {PLAYABLE} = require('../constants/constants');
-const { KEY } = require("./constants/envConstants.js");
+const { KEY } = require("../constants/envConstants.js");
 
 router.get('/:key/user', async (req, res) => {
     if (req.params.key !== KEY) {
